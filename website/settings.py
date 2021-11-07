693c1fa16a526b1e1d7fdd6ec8812a65b6daa9f3
bl4cknull <mohammadradmehr@hotmail.com>
2021-11-07 05:50:48 +0330

    qwe

--- website/settings.py
+++ website/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 django_heroku.settings(locals())
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
